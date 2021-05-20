fdc02477c8fe8af3ae76b44c6253a32834cf8f6d
jrozal <68182930+jrozal@users.noreply.github.com>
2021-05-19 22:42:22 -0700

    Update get routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const controller = require('../controllers');
 
-router.get('/getIndexData', controller.getIndexData);
+router.get('/getIndexData', controller.getMarketIndexData);
+
+router.get('/getPortfolioData', controller.getPortfolioData);
 
 module.exports = router;
