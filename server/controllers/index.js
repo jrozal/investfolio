f3185f1d43d37a13711b2c0f5ebd689ed9ff74f3
jrozal <68182930+jrozal@users.noreply.github.com>
2021-05-27 21:10:47 -0700

    Refactor out controller helper functions

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,42 +2,16 @@ const api = require('../api');
 const { models } = require('../../database/');
 const { calculatePortfolioData } = require('../helpers');
 
-const getMarketIndexData = (req, res) => {
+function getMarketIndexData(req, res) {
   api.getMarketIndexData()
     .then(data => res.status(200).send(data))
     .catch(err => res.status(500).send(err))
 };
 
-const getPortfolioData = (req, res) => {
-  const apiResponse = models.portfolio
-    .findAll({
-      order: [
-        ['symbol', 'ASC']
-      ],
-      attributes: ['symbol'],
-      raw: true
-    })
-    .then(data => {
-      let symbols = [];
-      data.forEach((record) => {
-        symbols.push(record.symbol)
-      });
-      return symbols;
-    })
-    .then((symbols) => {
-      return api.getPortfolioData(symbols);
-    })
-    .catch(err => err);
-
-  const queryResponse = models.portfolio
-    .findAll({
-      order: [
-        ['symbol', 'ASC']
-      ],
-      raw: true
-    })
-    .then(data => data)
-    .catch(err => err)
+async function getPortfolioData(req, res) {
+  const symbols = await getPortfolioSymbolsFromDb();
+  const apiResponse = await getLiveMarketData(symbols);
+  const queryResponse = await getPortfolioDataFromDb();
 
   Promise.all([apiResponse, queryResponse])
     .then(([apiData, queryData]) => {
@@ -46,5 +20,38 @@ const getPortfolioData = (req, res) => {
     .then(data => res.status(200).send(data))
     .catch(err => res.status(500).send(err))
 };
+
+function getPortfolioDataFromDb() {
+  return models.portfolio
+    .findAll({
+      order: [
+        ['symbol', 'ASC']
+      ],
+      raw: true
+    })
+    .then(data => data)
+    .catch(err => err)
+};
+
+function getPortfolioSymbolsFromDb() {
+  return models.portfolio
+    .findAll({
+      order: [
+        ['symbol', 'ASC']
+      ],
+      attributes: ['symbol'],
+      raw: true
+    })
+    .then(data => {
+      return data.map((record) => {
+        return record.symbol
+      })
+    })
+    .catch(err => err);
+};
+
+function getLiveMarketData(symbols) {
+  return api.getPortfolioData(symbols);
+}
 
 module.exports = { getMarketIndexData, getPortfolioData };
