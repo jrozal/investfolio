fe6ba30937533995655a87610d4232f0bf74c689
jrozal <68182930+jrozal@users.noreply.github.com>
2021-06-01 19:50:29 -0700

    Require body-parser middleware on server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
 const router = require('./routes');
+
+app.use(bodyParser.json());
 
 // establish database connection
 const db = require('../database');
