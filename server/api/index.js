d652c28d7b50a49c1317670b897b9bf24ef160b6
jrozal <68182930+jrozal@users.noreply.github.com>
2021-06-02 18:49:06 -0700

    Refactor helper function

--- server/api/index.js
+++ server/api/index.js
@@ -3,48 +3,49 @@ const config = require('../../config');
 const { twelvedataApiKey, finnhubApiKey } = config;
 const { parseIndexData } = require('../helpers');
 
-const twelvedataURL = `https://api.twelvedata.com/time_series?symbol=SPX, NDX, RUT, BTC/USD&interval=1day&outputsize=2&apikey=${twelvedataApiKey}`;
-const finnhubURL = 'https://finnhub.io/api/v1';
+function getMarketIndexData() {
+  const twelvedataURL = `https://api.twelvedata.com/time_series?symbol=SPX, NDX, RUT, BTC/USD&interval=1day&outputsize=2&apikey=${twelvedataApiKey}`;
 
-const getMarketIndexData = () => {
-  console.log('requesting index data')
   return axios.get(twelvedataURL)
     .then(res => parseIndexData(res.data))
     .catch(err => err)
 };
 
-const getPortfolioData = (symbols) => {
-  console.log('requesting stock data');
-  let reqestUrls = [];
-  let to = Math.floor((new Date().getTime()) / 1000); // unix time in seconds
-  let from = to - 86400; // 24 hrs ago
+function generateFinnhubUrls(symbols) {
+  const finnhubURL = 'https://finnhub.io/api/v1';
+  const to = Math.floor((new Date().getTime()) / 1000); // unix time in seconds
+  const from = to - 86400; // 24 hrs ago
+  let urls = [];
 
   symbols.forEach((symbol) => {
     if (symbol.includes('USD')) {
       let newSymbol = symbol.split('/').join('') + 'T';
-      reqestUrls.push(
+      urls.push(
         axios.get(`${finnhubURL}/crypto/candle?symbol=BINANCE:${newSymbol}&resolution=D&from=${from}&to=${to}&token=${finnhubApiKey}`)
-      )
+      );
     } else {
-      reqestUrls.push(
+      urls.push(
         axios.get(`${finnhubURL}/quote?symbol=${symbol}&token=${finnhubApiKey}`)
-      )
+      );
     }
   });
 
-  return Promise.all(reqestUrls)
+  return urls;
+};
+
+function getPortfolioData(symbols) {
+  const requestUrls = generateFinnhubUrls(symbols);
+
+  return Promise.all(requestUrls)
     .then(res => {
       let results = [];
 
       symbols.forEach((symbol, i) => {
-        results.push(
-          {
-            symbol: symbol,
-            data: res[i].data
-          }
-        )
+        results.push({
+          symbol: symbol,
+          data: res[i].data
+        })
       })
-
       return results;
     })
     .catch(err => console.log(err))
