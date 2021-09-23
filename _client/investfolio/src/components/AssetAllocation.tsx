d8e816ef2a8780acbd8cae1206d6b9f07cc88498
jrozal <68182930+jrozal@users.noreply.github.com>
2021-09-22 22:34:13 -0700

    Add AssetAllocation component

---
+++ _client/investfolio/src/components/AssetAllocation.tsx
@@ -0,0 +1,57 @@
+import { Box, Card, CardContent, CardHeader, Divider, Grid } from "@material-ui/core";
+import { Pie } from 'react-chartjs-2';
+
+
+const AssetAllocation = () => {
+  const data = {
+    labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
+    datasets: [
+      {
+        data: [20, 20, 20, 20, 20],
+        backgroundColor: [
+          '#E7F09F',
+          '#5760A0',
+          '#8B94C0',
+          '#07B088',
+          '#12123D',
+        ],
+        hoverOffset: 4
+      }
+    ]
+  };
+
+  const options = {
+    layout: { padding: 0 },
+    legend: {
+      display: false
+    },
+    maintainAspectRatio: false,
+    responsive: true,
+  };
+
+  return (
+    <Grid
+      item
+      lg={6}
+      md={6}
+      xl={3}
+      xs={12}
+    >
+      <Card>
+        <CardHeader title="Asset Allocation"/>
+          <Divider/>
+          <CardContent>
+            <Box>
+              <Pie
+                data={data}
+                height={400}
+                options={options}
+              />
+            </Box>
+          </CardContent>
+      </Card>
+    </Grid>
+  )
+};
+
+export default AssetAllocation;
