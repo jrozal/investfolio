344404140dbbd07095634a9198844a075edc9fbf
jrozal <68182930+jrozal@users.noreply.github.com>
2021-09-22 21:42:57 -0700

    Style component with MaterialUI Grid

--- _client/investfolio/src/components/Investments.tsx
+++ _client/investfolio/src/components/Investments.tsx
@@ -3,6 +3,7 @@ import {
   Card,
   CardHeader,
   Divider,
+  Grid,
   Table,
   TableBody,
   TableCell,
@@ -15,24 +16,32 @@ const Investments = () => {
   const exampleData = ['SPY', 'SPDR S&P 500 ETF', '441.40', '−4.31', '-0.97%', '-4.31', '1'];
 
   return (
-    <Card>
-      <CardHeader title="Portfolio"/>
-      <Divider/>
-      <Box minWidth={800}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              {headings.map((heading) => <TableCell>{heading}</TableCell>)}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <TableRow hover>
-              {exampleData.map((value) => <TableCell>{value}</TableCell>)}
-            </TableRow>
-          </TableBody>
-        </Table>
-      </Box>
-    </Card>
+    <Grid
+      item
+      lg={12}
+      md={12}
+      xl={9}
+      xs={12}
+    >
+      <Card>
+        <CardHeader title="Portfolio"/>
+        <Divider/>
+        <Box minWidth={800}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                {headings.map((heading) => <TableCell>{heading}</TableCell>)}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow hover>
+                {exampleData.map((value) => <TableCell>{value}</TableCell>)}
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Box>
+      </Card>
+    </Grid>
   )
 };
 
