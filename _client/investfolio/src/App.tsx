1c2d8f67c966f2f01a3e5bfa58eda84d8b7edee9
jrozal <68182930+jrozal@users.noreply.github.com>
2021-09-22 23:44:28 -0700

    Complete refactor of client to Typescript using temporary data

--- _client/investfolio/src/App.tsx
+++ _client/investfolio/src/App.tsx
@@ -1,9 +1,74 @@
-function App() {
+import { Box, Container, CssBaseline, Grid, makeStyles } from "@material-ui/core";
+import { ThemeProvider, createTheme } from "@material-ui/core/styles";
+import Investments from "./components/Investments";
+import IndexCards from './components/IndexCards';
+import Navbar from "./components/Navbar";
+import AssetAllocation from "./components/AssetAllocation";
+import PercentReturn from "./components/PercentReturn";
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: '#f2f2f2'
+    }
+  },
+  typography: {
+    h5: {
+      fontSize: '1.25rem'
+    }
+  }
+});
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    height: '100%',
+    overflow: 'hidden',
+    width: '90%',
+    margin: 'auto'
+  },
+  wrapper: {
+    display: 'flex',
+    flex: '1 1 auto',
+    overflow: 'hidden',
+    paddingTop: 64
+  },
+  content: {
+    display: 'flex',
+    flex: '1 1 auto',
+    overflow: 'hidden',
+    paddingTop: 24,
+    paddingBottom: 24
+  },
+});
+
+
+const App = () => {
+  const classes = useStyles();
+  const tempData = [-2, -1, 0.5, 1];
+
   return (
-    <div>
-      Hello World
-    </div>
-  );
-}
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Navbar/>
+      <div className={classes.root}>
+        <div className={classes.wrapper}>
+          <div className={classes.content}>
+            <Box>
+              <Container>
+                <Grid container spacing={3}>
+                  <IndexCards marketData={tempData} />
+                  <AssetAllocation />
+                  <PercentReturn />
+                  <Investments />
+                </Grid>
+              </Container>
+            </Box>
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  )
+};
 
 export default App;
