9c6c886ca45db26722184bf6579dbc197791a744
jrozal <68182930+jrozal@users.noreply.github.com>
2021-09-23 17:17:03 -0700

    Use cors middleware

--- server/index.js
+++ server/index.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-const bodyParser = require('body-parser');
+const cors = require('cors');
+const port = 3001;
 const router = require('./routes');
 
-app.use(bodyParser.json());
+app.use(cors({ origin: '*' }));
 
 // establish database connection
-const db = require('../database');
+const db = require('./database');
 
 // server routes
 app.use('/', router);
-
-// serve the client files
-app.use('/', express.static(__dirname + '/../client/dist'));
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
