cfde38abe04961e5ce908e54c4ca6992bb64d325
jrozal <68182930+jrozal@users.noreply.github.com>
2021-09-23 22:02:25 -0700

    Structure index data in array

--- server/helpers/index.js
+++ server/helpers/index.js
@@ -1,17 +1,18 @@
 // parse data for client
 const parseIndexData = (data) => {
-  let newData = {};
+  let newData = [];
 
   for (stock in data) {
     let today = parseFloat(data[stock].values[0].close).toFixed(2);
     let yesterday = parseFloat(data[stock].values[1].close).toFixed(2);
     let change = (((today - yesterday) / yesterday) * 100).toFixed(2) + '%';
 
-    newData[stock] = {
+    newData.push({
+      symbol: stock,
       today: today,
       yesterday: yesterday,
       change: change
-    }
+    });
   }
 
   return newData;
