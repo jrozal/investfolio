c65263e867c361817f5064f6fcbd338b2a61d242
jrozal <68182930+jrozal@users.noreply.github.com>
2021-09-28 23:38:37 -0700

    Refactor api calls to use coinbase pro for cryptocurrency data

--- server/api/index.js
+++ server/api/index.js
@@ -1,27 +1,45 @@
 const axios = require('axios');
 const config = require('../config');
-const { twelvedataApiKey, finnhubApiKey } = config;
+const { finnhubApiKey } = config;
 const { parseIndexData } = require('../helpers');
+const finnhubURL = 'https://finnhub.io/api/v1';
 
 function getMarketIndexData() {
-  const twelvedataURL = `https://api.twelvedata.com/time_series?symbol=SPX, NDX, RUT, BTC/USD&interval=1day&outputsize=2&apikey=${twelvedataApiKey}`;
+  const symbols = ['SPY', 'QQQ', 'IWM', 'BTC/USD'];
 
-  return axios.get(twelvedataURL)
-    .then(res => parseIndexData(res.data))
+  const urls = symbols.map((symbol) => {
+    if (symbol !== 'BTC/USD') {
+      return (axios.get(`${finnhubURL}/quote?symbol=${symbol}&token=${finnhubApiKey}`));
+    } else {
+      return (axios.get('https://api.pro.coinbase.com/products/BTC-USD/stats'))
+    }
+  });
+
+  return Promise.all(urls)
+    .then(results => {
+      return results.map((res, i) => {
+        return {
+          symbol: symbols[i],
+          ...res.data
+        }
+      });
+    })
+    .then(data => parseIndexData(data))
     .catch(err => err)
 };
 
 function generateFinnhubUrls(symbols) {
-  const finnhubURL = 'https://finnhub.io/api/v1';
   const to = Math.floor((new Date().getTime()) / 1000); // unix time in seconds
   const from = to - 86400; // 24 hrs ago
   let urls = [];
 
   symbols.forEach((symbol) => {
     if (symbol.includes('USD')) {
-      let newSymbol = symbol.split('/').join('') + 'T';
+      // let newSymbol = symbol.split('/').join('') + 'T';
+      let newSymbol = symbol.split('/').join('-');
       urls.push(
-        axios.get(`${finnhubURL}/crypto/candle?symbol=BINANCE:${newSymbol}&resolution=D&from=${from}&to=${to}&token=${finnhubApiKey}`)
+        // axios.get(`${finnhubURL}/crypto/candle?symbol=BINANCE:${newSymbol}&resolution=D&from=${from}&to=${to}&token=${finnhubApiKey}`)
+        axios.get(`https://api.pro.coinbase.com/products/${newSymbol}/stats`)
       );
     } else {
       urls.push(
@@ -46,6 +64,7 @@ function getPortfolioData(symbols) {
           data: res[i].data
         })
       })
+      console.log(results)
       return results;
     })
     .catch(err => console.log(err))
