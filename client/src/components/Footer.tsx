f4781660fd19349b0c769b6af40b917689aa7e4e
jrozal <68182930+jrozal@users.noreply.github.com>
2021-09-28 22:09:13 -0700

    Add footer

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,29 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  footer: {
+    display: 'flex',
+    float: 'right',
+    bottom: 0,
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingRight: 15,
+    color: '#3c4858'
+  }
+})
+
+const Footer = () => {
+  const classes = useStyles();
+  let date = new Date();
+  let year = date.getFullYear();
+
+  return (
+    <footer className={classes.footer}>
+      <span>
+        &copy; {year} <a href="https://justinrozal.dev" target="_blank" rel="noopener noreferrer" aria-label="Open personal portfolio for Justin Rozal">Justin Rozal</a>
+      </span>
+    </footer>
+  )
+};
+
+export default Footer;
