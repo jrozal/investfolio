c8f0436230d293f4291257d780a5832fe65eb2db
jrozal <68182930+jrozal@users.noreply.github.com>
2021-09-30 20:22:59 -0700

    Move theme into themes directory

---
+++ client/src/themes/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@material-ui/core";
+
+export const theme = createTheme({
+  palette: {
+    background: {
+      default: '#f2f2f2'
+    }
+  },
+  typography: {
+    h5: {
+      fontSize: '1.25rem'
+    }
+  }
+});
