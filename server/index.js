38d86ba0c991d3bd9d61379a8033f92cebb5f7fb
jrozal <68182930+jrozal@users.noreply.github.com>
2021-10-03 21:38:20 -0700

    Update server routes and cors origin property

--- server/index.js
+++ server/index.js
@@ -4,13 +4,13 @@ const cors = require('cors');
 const port = 3001;
 const router = require('./routes');
 
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 // establish database connection
 const db = require('./database');
 
 // server routes
-app.use('/', router);
+app.use('/api', router);
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
