7a770b69aa3115bf3b4836e2d12c8a82bc6a1230
jrozal <68182930+jrozal@users.noreply.github.com>
2021-10-13 21:24:19 -0700

    Add body-parser middleware

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const port = 3001;
 const router = require('./routes');
 
 app.use(cors({ origin: 'http://localhost:3000' }));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 // establish database connection
 const db = require('./database');
