b76b2f0e7cdef5c85890e1b4fce2c817d4a8f07c
jrozal <68182930+jrozal@users.noreply.github.com>
2021-10-17 19:10:23 -0700

    Rename styles directory

---
+++ client/src/styles/theme.ts
@@ -0,0 +1,20 @@
+import { createTheme } from "@material-ui/core";
+
+export const theme = createTheme({
+  palette: {
+    background: {
+      default: '#F4F6FA'
+    }
+  },
+  typography: {
+    fontFamily: 'PT Sans, sans-serif',
+    h5: {
+      fontSize: '1.3rem',
+      fontWeight: 700
+    },
+    h6: {
+      fontSize: '1.3rem',
+      fontWeight: 700
+    },
+  }
+});
