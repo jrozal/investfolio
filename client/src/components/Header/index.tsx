7f81d15353f926d80963ba67f5f59842c0b18138
jrozal <68182930+jrozal@users.noreply.github.com>
2021-10-19 22:21:04 -0700

    Center header title

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -6,9 +6,10 @@ const useStyles = makeStyles({
     boxShadow: `0 0 8px rgba(0,0,0,0.11)`
   },
   toolBar: {
-    height: '6rem'
+    height: '6rem',
+    display: 'flex',
+    justifyContent: 'center'
   },
-
   heading: {
     fontFamily: "'Pacifico', cursive !important",
     fontSize: '1.7rem !important',
@@ -25,8 +26,6 @@ const Header = () => {
       <Toolbar className={classes.toolBar}>
         <Typography>
           <span className={classes.heading}>investfolio</span>
-          <span>Total Assets</span>
-          <span>Today's Change</span>
         </Typography>
       </Toolbar>
     </AppBar>
