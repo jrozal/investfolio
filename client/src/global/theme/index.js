e86f783613352e663887018cbaa72ce9e2b2a22c
jrozal <68182930+jrozal@users.noreply.github.com>
2021-11-08 18:54:01 -0800

    Add Card component styling to global theme

--- client/src/global/theme/index.js
+++ client/src/global/theme/index.js
@@ -4,17 +4,31 @@ export const theme = createTheme({
   palette: {
     background: {
       default: "#F4F6FA",
-    }
+    },
   },
   typography: {
-    fontFamily: 'Lato, sans-serif',
+    fontFamily: "Lato, sans-serif",
     h5: {
-      fontSize: '1.3rem',
-      fontWeight: 700
+      fontSize: "1.3rem",
+      fontWeight: 700,
     },
     h6: {
-      fontSize: '1.3rem',
-      fontWeight: 700
+      fontSize: "1.3rem",
+      fontWeight: 700,
     },
-  }
+  },
+  components: {
+    MuiCard: {
+      variants: [
+        {
+          props: { variant: "rounded-soft" },
+          style: {
+            borderRadius: "12px",
+            padding: "5px 20px 20px 20px",
+            boxShadow: "0 0 8px rgba(0,0,0,0.11)",
+          },
+        },
+      ],
+    },
+  },
 });
