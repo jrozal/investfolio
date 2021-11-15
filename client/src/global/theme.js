a47e45da28047225e900e1c218513be229bc9662
jrozal <68182930+jrozal@users.noreply.github.com>
2021-11-14 16:55:40 -0800

    Adjust mui theme breakpoints

--- client/src/global/theme.js
+++ client/src/global/theme.js
@@ -1,6 +1,15 @@
 import { createTheme } from '@mui/material/styles';
 
 export const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    }
+  },
   palette: {
     background: {
       default: "#F4F6FA",
