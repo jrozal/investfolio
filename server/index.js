cb21279b949bae833611e875aeecd4a6a7e9c0d6
jrozal <68182930+jrozal@users.noreply.github.com>
2021-11-18 18:21:37 -0800

    Prepare repo for deployment

--- server/index.js
+++ server/index.js
@@ -4,6 +4,6 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000; // Heroku will need the PORT environment variable
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "..", "/client/build")));
 
 app.listen(port, () => console.log(`App is live on port ${port}!`));
